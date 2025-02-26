b60828745cc37af80447285d9f013c878ec3e708
KunalMT16 <thuturkarkunal@gmail.com>
2025-02-26 11:25:50 +0530

    Fixed navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,134 +1,138 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
 "use client";
 
 import * as React from "react";
 import Link from "next/link";
-
 import { cn } from "@/lib/utils";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger
 } from "@/components/ui/navigation-menu";
 // import { Search } from "lucide-react"
 import Searchbar from "../search";
 // import Searchbar from "../search/searchbar"
 
 const components = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response."
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description: "For sighted users to preview content available behind a link."
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar."
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content."
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time."
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."
-  }
+    {
+        title: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+            "A modal dialog that interrupts the user with important content and expects a response."
+    },
+    {
+        title: "Hover Card",
+        href: "/docs/primitives/hover-card",
+        description: "For sighted users to preview content available behind a link."
+    },
+    {
+        title: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar."
+    },
+    {
+        title: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content."
+    },
+    {
+        title: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+            "A set of layered sections of content—known as tab panels—that are displayed one at a time."
+    },
+    {
+        title: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."
+    }
 ];
 
 export default function Header() {
-  return (
-    <div className="flex flex-col p-5 m-auto">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem className="flex items-center justify-between w-full relative">
-            <NavigationMenuLink>
-              <Link href="/" className="inline-block text-xl font-bold">
-                Electronics Products
-              </Link>
-            </NavigationMenuLink>
+    return (
+        <header>
+            <nav nav className="w-full fixed h-16 bg-white shadow-md p-4 flex justify-between items-center z-50">
+                <div className="flex fixed flex-col p-5 m-auto">
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem className="flex items-center justify-between w-full relative">
+                                <NavigationMenuLink>
+                                    <Link href="/" className="inline-block text-xl font-bold">
+                                        Electronics Products
+                                    </Link>
+                                </NavigationMenuLink>
 
-            <NavigationMenuLink className="ml-5">
-              <Link
-                href="/"
-                className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
-              >
-                Home
-              </Link>
-            </NavigationMenuLink>
+                                <NavigationMenuLink className="ml-5">
+                                    <Link
+                                        href="/Home"
+                                        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
+                                    >
+                                        Home
+                                    </Link>
+                                </NavigationMenuLink>
 
-            <NavigationMenuLink>
-              <Link
-                href="/"
-                className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent "
-              >
-                Apparel
-              </Link>
-            </NavigationMenuLink>
+                                <NavigationMenuLink>
+                                    <Link
+                                        href="/Apparel"
+                                        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent "
+                                    >
+                                        Apparel
+                                    </Link>
+                                </NavigationMenuLink>
 
-            <NavigationMenuLink>
-              <Link
-                href="/"
-                className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
-              >
-                Accessories
-              </Link>
-            </NavigationMenuLink>
+                                <NavigationMenuLink>
+                                    <Link
+                                        href="/Accessories"
+                                        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
+                                    >
+                                        Accessories
+                                    </Link>
+                                </NavigationMenuLink>
 
-            <NavigationMenuLink>
-              <Link
-                href="/"
-                className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
-              >
-                Digital
-              </Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink className="absolute left">
-              <Searchbar />
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  );
+                                <NavigationMenuLink>
+                                    <Link
+                                        href="/Digital"
+                                        className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent"
+                                    >
+                                        Digital
+                                    </Link>
+                                </NavigationMenuLink>
+
+                                <NavigationMenuLink>
+                                    <Searchbar />
+                                </NavigationMenuLink>
+
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+            </nav>
+        </header>
+    );
 }
 const ListItem = ({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    );
 };
 ListItem.displayName = "ListItem";
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,30 +1,29 @@
-import React from "react";
-import { Input } from "@/components/ui/input";
-import { Link, Search, ShoppingBag, UserCheck2Icon } from "lucide-react";
+import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Search, ShoppingBag, UserCheck2Icon } from 'lucide-react'
 
 function Searchbar() {
-  return (
-    <>
-      <div className="flex gap-4 ml-100">
-        <div className="relative">
-          <Input
-            type="search"
-            id="search"
-            placeholder="Search for Products..."
-            className="flex text-black w-60 h-8 border-input p-3 shadow-xs rounded-md border bg-white"
-          />
-          <Search className="absolute top-1.5 right-2 w-5 h-5" />
-        </div>
-        <div className="gap-1 flex">
-          <div className="flex top-8 m-auto pr-2 cursor-pointer">
-            <ShoppingBag />
-          </div>
-          <div className="flex top-8 m-auto pr-3 cursor-pointer">
-            <UserCheck2Icon />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <header className="fixed top-4 right-5 flex items-center gap-6 z-50">
+                <div className="relative hidden sm:flex">
+                    <Input
+                        type="search"
+                        id="search"
+                        placeholder="Search for Products..."
+                        className="text-black w-60 h-9 border-input p-3 shadow-xs rounded-md border bg-white"
+                    />
+                    <Search className="absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer" />
+                </div>
+                <div className="flex items-center gap-4">
+                    <ShoppingBag className="cursor-pointer" />
+                    <UserCheck2Icon className="cursor-pointer" />
+                </div>
+                <div className="sm:hidden">
+                    <Search className="cursor-pointer text-xl" />
+                </div>
+            </header>
+        </>
+    )
 }
-export default Searchbar;
+export default Searchbar
