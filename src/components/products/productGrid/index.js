ae16cb10ecdcafc6507f6ab45ab9814d35341018
imshn <shaan@vyomr.in>
2025-02-27 12:10:20 +0530

    index.js in productGrid

--- src/components/products/productGrid/index.js
+++ src/components/products/productGrid/index.js
@@ -5,31 +5,6 @@ import Image from "next/image";
 
 const ProductGrid = ({ title, price, cover }) => {
   return (
-<<<<<<< Updated upstream
-    <>
-      <main>
-        <div>
-          <Card className="overflow-hidden border-none shadow-none">
-            <div className="flex justify-center relative">
-              <CardHeader>
-                <Image width={700} height={700} src={cover} alt={title} className='rounded-lg cursor-pointer transition-all duration-200 hover:scale-110 hover:brightness-35' />
-              </CardHeader>
-              <div className='image-shadow'></div>
-            </div>
-            <div className="p-1 py-1">
-              <CardContent >
-                <h2 className="text-xl font-bold text-gray-700 cursor-pointer">{title}</h2>
-                <p>{price}</p>
-              </CardContent>
-            </div>
-          </Card>
-        </div>
-      </main>
-    </>
-  )
-
-}
-=======
     <Card className="overflow-hidden border-none shadow-none">
       <div className="flex justify-center relative">
         <CardHeader>
@@ -54,6 +29,5 @@ const ProductGrid = ({ title, price, cover }) => {
     </Card>
   );
 };
->>>>>>> Stashed changes
 
 export default ProductGrid;
