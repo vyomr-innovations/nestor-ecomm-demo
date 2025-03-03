cd95ea9c191099e5abe96ed43972c00853d421e3
KunalMT16 <thuturkarkunal@gmail.com>
2025-03-03 17:24:13 +0530

    Banner silder

--- src/app/products/[product]/page.js
+++ src/app/products/[product]/page.js
@@ -10,7 +10,7 @@ function ProductPage() {
             <div className="mt-16 pt-2">
                 <BreadCrumb
                     page1={"All Products"}
-                    catogory={"Sunbeam Tote Jeff"}
+                    page3={"Sunbeam Tote Jeff"}
                 />
             </div>
             <ImageView/>
